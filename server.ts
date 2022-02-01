68df2fc93a428f7ca94496c53548f6d432ae2699
Rishi <anotherishi@gmail.com>
2022-02-02 00:29:59 +0530

    test server2

--- server.ts
+++ server.ts
@@ -1,26 +1,7 @@
-const server = Deno.listen({ port: 80 });
-console.log(`HTTP webserver running.  Access it at:  http://localhost:80/`);
+import { serve } from "https://deno.land/std@0.123.0/http/server.ts";
 
+serve(handler);
 
-for await (const conn of server) {
-  serveHttp(conn);
-}
-
-async function serveHttp(conn: Deno.Conn) {
-  // This "upgrades" a network connection into an HTTP connection.
-  const httpConn = Deno.serveHttp(conn);
-  // Each request sent over the HTTP connection will be yielded as an async
-  // iterator from the HTTP connection.
-  for await (const requestEvent of httpConn) {
-    // The native HTTP server uses the web standard `Request` and `Response`
-    // objects.
-    const body = `Your user-agent is:\n\n${requestEvent.request.headers.get(
-      "user-agent",
-    ) ?? "Unknown"}`;
-    requestEvent.respondWith(
-      new Response(body, {
-        status: 200,
-      }),
-    );
+function handler(req: Request): Response {
+    return new Response("Hello, World! " + req.url);
   }
-}
